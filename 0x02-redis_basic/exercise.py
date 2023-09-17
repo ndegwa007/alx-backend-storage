a7aa4de7eac56a347291c0127608311e93d28138
ndegz <ndegwa8781@gmail.com>
2023-09-17 12:43:57 +0300

    redis cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,41 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        # Store the input arguments as a string in Redis
+        self._redis.rpush(input_key, str(args))
+
+        # Execute the wrapped function to retrieve the output
+        result = method(self, *args, **kwargs)
+
+        # Store the output in Redis
+        self._redis.rpush(output_key, str(result))
+
+        return result
+
+    return wrapper
+
+
+def count_calls(fn: Callable) -> Callable:
+    @wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        key = fn.__qualname__
+        result = self._redis.get(key)
+        if result is None:
+            result = 1
+        else:
+            result = int(result) + 1
+        self._redis.set(key, str(result).encode('utf-8'))
+        return str(result).encode('utf-8')
+    return wrapper
 
 
 class Cache:
@@ -12,6 +47,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """returns key to the data"""
         key = uuid.uuid4()
@@ -33,6 +69,6 @@ class Cache:
         """convert to str"""
         return self.get(key, fn=lambda d: d.decode('utf-8'))
 
-    def get_int():
+    def get_int(key: str) -> int:
         """convert to int"""
         return self.get(key, fn=lambda d: int(d))
